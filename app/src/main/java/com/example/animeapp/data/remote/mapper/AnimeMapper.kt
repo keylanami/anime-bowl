b287bd33037c9561ebd433f1379ef089126f92f5
klyroe <keylaanmrr@gmail.com>
2026-05-07 03:29:24 +0700

    add adjustment into anime and favanime mapper

--- app/src/main/java/com/example/animeapp/data/remote/mapper/AnimeMapper.kt
+++ app/src/main/java/com/example/animeapp/data/remote/mapper/AnimeMapper.kt
@@ -1,16 +1,21 @@
 package com.example.animeapp.data.remote.mapper
 
+import com.example.animeapp.data.local.entity.FavAnimeEntity
 import com.example.animeapp.data.model.Anime
 import com.example.animeapp.data.remote.dto.AnimeDTO
 
 fun AnimeDTO.toAnime(): Anime{
     return Anime(
-        id = mal_id,
+        id = 0,
+        mal_id = mal_id,
         title = title,
         type = type?: "unknown",
         episodes = episodes?: 0,
         rank = rank?: 0,
         score = score?: 0.0,
-        image_url = images.jpg.image_url
+        image_url = images.jpg.image_url,
+        status = "Plan to watch",
+        userNote = ""
     )
 }
+
--- app/src/main/java/com/example/animeapp/data/remote/mapper/FavAnimeMapper.kt
+++ app/src/main/java/com/example/animeapp/data/remote/mapper/FavAnimeMapper.kt
@@ -6,23 +6,29 @@ import com.example.animeapp.data.model.Anime
 fun Anime.toFavAnimeEntity(): FavAnimeEntity {
     return FavAnimeEntity(
         id = id,
+        mal_id = mal_id,
         title = title,
         type = type,
         episodes = episodes,
         score = score,
         rank = rank,
-        image_url = image_url
+        image_url = image_url,
+        status = status,
+        user_note = userNote
     )
 }
 
 fun FavAnimeEntity.toAnime(): Anime {
     return Anime (
         id = id,
+        mal_id = mal_id,
         title = title,
         type = type,
         episodes = episodes,
         score = score,
         rank = rank,
-        image_url = image_url
+        image_url = image_url,
+        status = status,
+        userNote = user_note
     )
 }
