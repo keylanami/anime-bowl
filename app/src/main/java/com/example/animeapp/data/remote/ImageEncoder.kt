95410c02b9ab3d4b42e66a81043570ccaa1ec3da
klyroe <keylaanmrr@gmail.com>
2026-06-24 23:07:41 +0700

    add auto resize due to file size kegedean error

--- app/src/main/java/com/example/animeapp/data/remote/ImageEncoder.kt
+++ app/src/main/java/com/example/animeapp/data/remote/ImageEncoder.kt
@@ -10,10 +10,31 @@ import java.io.ByteArrayOutputStream
 fun encodeImageUriToBase64(context: Context, uri: Uri): String? {
     return try {
         val inputStream = context.contentResolver.openInputStream(uri)
-        val bitmap = BitmapFactory.decodeStream(inputStream)
+        val originalBitmap = BitmapFactory.decodeStream(inputStream)
+
+        val maxWidth = 800
+        val maxHeight = 800
+        var width = originalBitmap.width
+        var height = originalBitmap.height
+
+        if (width > maxWidth || height > maxHeight) {
+            val ratioBitmap = width.toFloat() / height.toFloat()
+            val ratioMax = maxWidth.toFloat() / maxHeight.toFloat()
+
+            if (ratioMax > ratioBitmap) {
+                width = (maxHeight * ratioBitmap).toInt()
+                height = maxHeight
+            } else {
+                width = maxWidth
+                height = (maxWidth / ratioBitmap).toInt()
+            }
+        }
+        val scaledBitmap = Bitmap.createScaledBitmap(originalBitmap, width, height, true)
+
         val outputStream = ByteArrayOutputStream()
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 50, outputStream)
+        scaledBitmap.compress(Bitmap.CompressFormat.JPEG, 60, outputStream)
         val byteArray = outputStream.toByteArray()
+
         "data:image/jpeg;base64," + Base64.encodeToString(byteArray, Base64.DEFAULT)
     } catch (e: Exception) {
         null
